38d7d797af785f61d32076c3fe586799f1b6fb49
yanick <bertrandpacode@icloud.com>
2020-09-17 16:42:01 +0200

    ajout d'un deuxieme system.out

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bonjour");
+		for(i=0,i<10,i++)
+			System.out.println("maman");
 
 	}
 
